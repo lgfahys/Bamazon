d9371d5717c888def7ef16ef9e9a4cffcc866337
lgfahys <lindsey@codeasite.com>
2019-04-26 15:35:44 -0400

    created working function to update the stock in the database based on what was purchased

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,50 +12,77 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     chooseProduct();
-  });
+});
 
 function chooseProduct() {
     console.log("\n" + "\033[38;5;1m" + "Searching all products..." + "\033[0m" + "\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      for (i = 0; i < res.length; i++) {
-        console.log("\033[38;5;6m" + "     Item ID: " + "\033[0m" + "\033[38;5;10m" + res[i].item_id + "\033[0m");
-        console.log("\033[38;5;6m" + "Product Name: " + "\033[0m" + res[i].product_name);
-        console.log("\033[38;5;6m" + "       Price: " + "\033[0m" + "$" + res[i].price + "\n");
-      }
-
-      var products = [];
-      for (i = 0; i < res.length; i++) {
-          let string = "";
-          string = "\033[38;5;10m" + res[i].item_id + "\033[0m" + " " + res[i].product_name;
-          products.push(string);
-      }
-
-      inquirer.prompt([
-
-        {
-            type: "list",
-            name: "itemChoice",
-            message: "What item would you like to buy?",
-            choices: products
-        },
-
-        {
-            type: "prompt",
-            name: "itemQuantity",
-            message: "What quantity would you like to purchase?",
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        for (i = 0; i < res.length; i++) {
+            console.log("\033[38;5;6m" + "     Item ID: " + "\033[0m" + "\033[38;5;10m" + res[i].item_id + "\033[0m");
+            console.log("\033[38;5;6m" + "Product Name: " + "\033[0m" + res[i].product_name);
+            console.log("\033[38;5;6m" + "       Price: " + "\033[0m" + "$" + res[i].price + "\n");
         }
 
-    ]).then(function (choice) {
-        // console.log(choices.indexOf(choice.itemChoice));
+        var products = [];
+        for (i = 0; i < res.length; i++) {
+            let string = "";
+            string = "\033[38;5;10m" + res[i].item_id + "\033[0m" + " " + res[i].product_name;
+            products.push(string);
+        }
 
-        console.log("");
+        inquirer.prompt([
+
+            {
+                type: "list",
+                name: "itemChoice",
+                message: "What item would you like to buy?",
+                choices: products
+            },
+
+            {
+                type: "prompt",
+                name: "itemQuantity",
+                message: "What quantity would you like to purchase?",
+            }
+
+        ]).then(function (choice) {
+            var itemIndex = products.indexOf(choice.itemChoice);
+            var itemId = itemIndex + 1;
+            var currentStock = res[itemIndex].stock;
+            var updatedStock = currentStock - choice.itemQuantity;
+            if (updatedStock >= 0) {
+                console.log("\n" + "\033[38;5;1m" + "Searching all product quantities..." + "\033[0m" + "\n");
+                var query = connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock: updatedStock
+                        },
+                        {
+                            item_id: itemId
+                        }
+                    ],
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log(res.affectedRows + " products updated!\n");
+                    }
+                );
+                console.log(query.sql);
+                console.log("You have purchased " + choice.itemQuantity + " of the product " + res[itemIndex].product_name);
+            } else {
+                console.log("\n" + "\033[38;5;1m" + "Insufficient Quantity! We only have " + "\033[0m" + currentStock + "\033[38;5;1m" + " of these items left" + "\033[0m");
+            }
+            // var itemIndex = products.indexOf(choice.itemChoice);
+            // console.log(res[itemIndex].stock - choice.itemQuantity);
+            // console.log(choice.itemQuantity);
+            // console.log(itemIndex);
+            connection.end();
+        });
     });
-      connection.end();
-    });
-  }
+}
 
