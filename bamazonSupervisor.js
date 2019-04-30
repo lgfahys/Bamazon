d21ca3b6365985838706b263a9815d83d6955d5c
lgfahys <lindsey@codeasite.com>
2019-04-30 12:20:39 -0400

    added basic inquirer to bamazonSupervisor.js

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -15,4 +15,33 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
+    superviseProducts();
 });
+
+function superviseProducts() {
+    console.log("\n" + "\033[38;5;1m" + "Welcome Bamazon Supervisor" + "\033[0m" + "\n");
+    inquirer.prompt([
+
+        {
+            type: "list",
+            name: "supervisorChoice",
+            message: "What would you like to do?",
+            choices: ["View Product Sales by Department", "Create New Department"]
+        }
+
+    ]).then(function (choice) {
+        switch (choice.supervisorChoice) {
+            case "View Product Sales by Department":
+                viewDepartmentSales();
+                break;
+
+            case "Create New Department":
+                createDepartment();
+                break;
+
+            case "Exit":
+                process.exit();
+                break;
+        }
+    });
+}
