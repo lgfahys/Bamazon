48cef6886cabb1edb547df2b88161c76c1da7c67
lgfahys <lindsey@codeasite.com>
2019-05-01 15:54:03 -0400

    completed createDepartment and viewDepartmentSales in bamazonSupervisor

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -48,10 +48,9 @@ function superviseProducts() {
 
 function viewDepartmentSales() {
 
-    var sqlQuery = "SELECT departments.department_id, departments.department_name, departments.over_head_costs, SUM(products.product_sales) AS 'product_sales', (SUM(products.product_sales) - departments.over_head_costs) AS 'total_profit' FROM departments INNER JOIN products ON departments.department_id = products.department_id GROUP BY department_name, departments.department_id, departments.over_head_costs";
+    var sqlQuery = "SELECT departments.department_id, departments.department_name, departments.over_head_costs, SUM(products.product_sales) AS 'product_sales', (SUM(products.product_sales) - departments.over_head_costs) AS 'total_profit' FROM departments INNER JOIN products ON departments.department_id = products.department_id GROUP BY department_name, departments.department_id, departments.over_head_costsme";
 
     connection.query(sqlQuery, function (err, res) {
-        // connection.query("SELECT * FROM departments", function (err, res) {
         if (err) throw err;
         var table = new Table({
             head: ['Department ID', 'Department Name', 'Over Head Costs', 'Product Sales', 'Total Profit'],
@@ -59,21 +58,48 @@ function viewDepartmentSales() {
 
         for (i = 0; i < res.length; i++) {
             table.push(
-                [ res[i].department_id, res[i].department_name, res[i].over_head_costs, res[i].product_sales, res[i].total_profit ]
+                [res[i].department_id, res[i].department_name, res[i].over_head_costs, res[i].product_sales, res[i].total_profit]
             );
         }
-         
+
         console.log(table.toString());
         // console.log(res);
         connection.end();
     });
 }
 
-// 5. The `total_profit` column should be calculated on the fly using the difference between 
-// `over_head_costs` and `product_sales`. `total_profit` should not be stored in any database. 
-// You should use a custom alias.
+function createDepartment() {
 
-// | department_id | department_name | over_head_costs | product_sales | total_profit |
-// | ------------- | --------------- | --------------- | ------------- | ------------ |
-// | 01            | Electronics     | 10000           | 20000         | 10000        |
-// | 02            | Clothing        | 60000           | 100000        | 40000        |
+    inquirer.prompt([
+        {
+            type: "prompt",
+            name: "departmentName",
+            message: "What would you like to name the new department?",
+        },
+
+        {
+            type: "prompt",
+            name: "departmentCosts",
+            message: "What are the overhead costs for this department?",
+        }
+    ]).then(function (choice) {
+        var departmentName = choice.departmentName;
+        var overheadCosts = choice.departmentCosts;
+            var query = connection.query(
+                "INSERT INTO departments SET ?",
+                [
+                    {
+                        department_name: departmentName,
+                        over_head_costs: overheadCosts
+                    }
+                ],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("The department " + departmentName + " has been added!");
+                }
+            );
+
+        connection.end();
+    });
+}
+
